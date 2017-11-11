4e91e281ba5ec416be1f86511a467b7942952291
michele-mada <michele.mm1234@gmail.com>
2017-11-11 22:50:32 +0100

    added compile script

---
+++ acompile_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+AOCFLAGS="--profile -v --no-interleaving"
+
+
+array=( "aes_swi_big" "aes_swi_medium" "aes_swi_small" "aes_swi_tiny" "des_swi_default" )
+for i in "${array[@]}"
+do
+    echo "Compiling kernel kernel $i"
+    aoc $AOCFLAGS "src_cl/$i.cl" -o "bin_cl/$i.aocx"
+done
