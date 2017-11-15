f93a16c61961b0f74ad4a37fd69ef3d90b8be93c
michele-mada <michele.mm1234@gmail.com>
2017-11-15 16:41:30 +0100

    improved plot script

--- plot_tuning_data.py
+++ plot_tuning_data.py
@@ -1,37 +1,67 @@
+#!/bin/python
+
 import matplotlib.pyplot as plt
 import sys
 import numpy as np
+import argparse
 
 repetitions = 100
 convert_to_megs = 1.0/1048576
 
-with open(sys.argv[1] or "autotune.txt", "r") as fp:
-    all_data = np.loadtxt(fp, delimiter="\t", skiprows=1)
 
-block_sizes = all_data[:,0]
-run_times = all_data[:,1]
+def parsecli():
+    parser = argparse.ArgumentParser(description="Visualize opencl_ciphers_improved_test tuning data")
+    parser.add_argument('-s','--save', help='Save the graph as an image', action="store_true")
+    parser.add_argument('-q','--quiet', help='Don\'t display/print anything (use with -s)', action="store_true")
+    parser.add_argument('file', metavar='filename', help='Text file with the tuning data', type=str, default="autotune.txt")
+    return parser.parse_args()
 
-throughput = (block_sizes * repetitions * convert_to_megs) / run_times
-best_throughput = max(throughput)
-best_blocksize = block_sizes[np.where(throughput == best_throughput)[0]]
 
-fig, ax = plt.subplots()
-plt.plot(block_sizes * convert_to_megs, throughput)
-plt.margins(y=0.2)
-stride = 4
-if max(block_sizes * convert_to_megs) > 100: stride = 40
-elif max(block_sizes * convert_to_megs) > 1000: stride = 80
-ax.set_xticks(np.arange(0, max(block_sizes * convert_to_megs), stride))
-plt.xlabel('block size (MB)')
-plt.ylabel('throughput (MB/s)')
-plt.title('Throughput tuning')
-plt.grid(True)
+def main(cli):
+    with open(cli.file, "r") as fp:
+        all_data = np.loadtxt(fp, delimiter="\t", skiprows=1)
 
-plt.annotate(
-    "Best throughput @ block_size=%uMB" % int(best_blocksize * convert_to_megs),
-    xy=(best_blocksize * convert_to_megs, best_throughput), xytext=(-20, 20),
-    textcoords='offset points', ha='left', va='bottom',
-    bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5),
-    arrowprops=dict(arrowstyle = '->', connectionstyle='arc3,rad=0'))
+    block_sizes = all_data[:,0]
+    run_times = all_data[:,1]
 
-plt.show()
+    throughput = (block_sizes * repetitions * convert_to_megs) / run_times
+    best_throughput = max(throughput)
+    best_blocksize = block_sizes[np.where(throughput == best_throughput)[0]]
+
+    if not cli.quiet:
+        print("Best throughput: %.3f MBs at blocksize: %uMB" % (best_throughput,int(best_blocksize * convert_to_megs)))
+
+    fig, ax = plt.subplots()
+    plt.plot(block_sizes * convert_to_megs, throughput)
+    plt.margins(y=0.2)
+    stride = 4
+    if max(block_sizes * convert_to_megs) > 100: stride = 40
+    elif max(block_sizes * convert_to_megs) > 1000: stride = 80
+    ax.set_xticks(np.arange(0, max(block_sizes * convert_to_megs), stride))
+    plt.xlabel('block size (MB)')
+    plt.ylabel('throughput (MB/s)')
+    plt.title('Throughput tuning')
+    plt.grid(True)
+    
+    labelpos = "left"
+    if best_blocksize > max(block_sizes)/2:
+        labelpos = "right"
+
+    plt.annotate(
+        "Best: %.3fMB/s @ bs=%uMB" % (best_throughput, int(best_blocksize * convert_to_megs)),
+        xy=(best_blocksize * convert_to_megs, best_throughput), xytext=(-20, 20),
+        textcoords='offset points', ha=labelpos, va='bottom',
+        bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5),
+        arrowprops=dict(arrowstyle = '->', connectionstyle='arc3,rad=0'))
+
+    figure = plt.gcf()
+    if not cli.quiet:
+        plt.show()
+    if cli.save:
+        plt.draw()
+        figure.savefig(cli.file + ".png", dpi=150)
+    
+    
+if __name__ == "__main__":
+    cli = parsecli()
+    main(cli)
